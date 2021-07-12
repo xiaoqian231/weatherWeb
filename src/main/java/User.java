2f2fdb3e31eb09281d4273f9a74d34364973d3bc
xiaoqian  gao <xiaoqian@student.agh.edu.pl>
2021-07-12 21:51:58 +0200

    Initial commit

---
+++ src/main/java/User.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private int ID;
+    private String password;
+    ArrayList<String> topic = new ArrayList<>();
+public  void addNewTopic(String a){
+
+   topic.add(a);
+}
+    public User() {
+    }
+
+    public User(String name, int ID, String password) {
+        this.name = name;
+        this.ID = ID;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", ID=" + ID +
+                ", password='" + password + '\'' +
+                ", arrayList=" + topic +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/UserDAO.java
@@ -0,0 +1,137 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class UserDAO {
+    private Connection connection=null;
+    private PreparedStatement preparedStatement=null;
+    private ResultSet resultSet=null;
+  /**
+   * creat table in SQL
+  * */
+    private void createTable ()throws SQLException{
+        String SQLCreate="CREATE TABLE IF NOT EXISIS USER"
+                +"(ID    int(11)     NOT NULL AUTO_INCREMENT,"
+                +"name text NOT NULL,"
+                +"password text NOT NULL,"
+                +"   PRIMARY KEY (`id`))";
+        connection = getConnection();
+        Statement stmt = connection.createStatement();
+        stmt.execute(SQLCreate;
+
+    }
+    /**
+     * method for login
+     * @param name
+     * @param password
+     * @return user
+    * */
+    public User login(String name,String password){
+        User user = new User();
+        try{
+            String query = "SELECT * FROM user where login = ? AND password = ?";
+            connection=getConnection();
+            preparedStatement= connection.prepareStatement(query);
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,password);
+            resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                user.setID(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
+        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }finally {
+            try{
+                if(connection!=null){connection.close();}
+                if(preparedStatement!=null){
+                    preparedStatement.close();
+                }
+            } catch (SQLException throwables){
+                throwables.printStackTrace();
+            }
+        }
+        return  user;
+    }
+
+    /**
+     * add user to database
+     * @param name
+     * @param password
+     * */
+    public boolean adduser (String name, String password){
+boolean result=false ;
+try{
+    String query ="SELECT login FROM user where login =?";
+    connection=getConnection();
+    preparedStatement= connection.prepareStatement(query);
+    preparedStatement.setString(1,name);
+    resultSet=preparedStatement.executeQuery();
+    if(!resultSet.next()){
+        String query2 = "INSERT INTO user(id, name, password) VALUES (NULL, ?, ?)";
+        preparedStatement = connection.prepareStatement(query2);
+
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, password);
+
+        preparedStatement.executeUpdate();
+        result = true;
+        System.out.println("User has been registered");
+    } else {
+        System.out.println("User already exists");
+    }
+}catch (SQLException throwables) {
+    throwables.printStackTrace();
+} finally {
+    try {
+        if (connection != null) {
+            connection.close();
+        }
+        if (preparedStatement != null) {
+            preparedStatement.close();
+        }
+    } catch (SQLException throwables) {
+        throwables.printStackTrace();
+    }
+}
+        return result;
+    }
+
+
+    /**
+     * topic list
+    * */
+    public ArrayList<String> getTopic() throws Exception{
+        ArrayList<String> getTopic = new ArrayList<>();
+
+        String sqlQuerry = "SELECT * FROM Topic";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/topic", "root", "");
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(sqlQuerry);
+        while (rs.next()) {
+
+            User user = new User();
+
+
+           User.setID(rs.getInt("id"));
+           user.addNewTopic("topic");
+
+       getTopic.add(User);
+        }
+        con.close();
+
+        return getTopic;
+
+    }
+
+
+
+    private Connection getConnection()throws SQLException{
+        Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/weatherWeb", "root", "");
+        return conn;
+    }
+}
